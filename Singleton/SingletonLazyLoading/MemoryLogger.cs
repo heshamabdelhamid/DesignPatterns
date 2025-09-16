700c0ce8c7482454cc5022862cc7a33a49a1fee3
heshamabdelhamid <heshamabdelhamid432@gmail.com>
2025-09-16 17:42:03 +0300

    add singleton by ThreadSafeUsingDoubkeCheckedLock and SingletonLazyLoading

---
+++ Singleton/SingletonLazyLoading/MemoryLogger.cs
@@ -0,0 +1,64 @@
+﻿namespace Singleton.SingletonLazyLoading;
+
+public class MemoryLogger
+{
+    private static readonly Lazy<MemoryLogger> _instance = new(() => new MemoryLogger());
+
+    //this if you want use EagerLoading
+    //private static readonly MemoryLogger _instance = new MemoryLogger();
+
+    private int _InfoCount;
+    private int _WarningCount;
+    private int _ErrorCount;
+
+    private List<LogMessage> _logs = [];
+    public IReadOnlyCollection<LogMessage> Logs => _logs;
+
+    private MemoryLogger() 
+    { 
+    }
+
+    public static MemoryLogger GetLogger
+    {
+        get
+        {
+            return _instance.Value;
+        }
+    }
+
+    private void Log(string message, LogType logType)
+    {
+        _logs.Add(new LogMessage
+        {
+            Message = message,
+            LogType = logType,
+            CreatedAt = DateTime.Now
+        });
+    }
+
+    public void LogInfo(string message)
+    {
+        ++_InfoCount;
+        Log(message, LogType.INFO);
+    }
+
+    public void LogError(string message)
+    {
+        ++_ErrorCount;
+        Log(message, LogType.ERROR);
+    }
+    
+    public void LogWarning(string message)
+    {
+        ++_WarningCount;
+        Log(message, LogType.WARNING);
+    }
+
+    public void ShowLog()
+    {
+        _logs.ForEach(x => Console.WriteLine(x));
+        Console.WriteLine($"-------------------------------");
+
+        Console.WriteLine($"Info ({_InfoCount}), Warning ({_WarningCount}), Error ({_ErrorCount})");
+    }
+}
