674734abd0d9f40ed3b9b92949b12a97110ced07
heshamabdelhamid <heshamabdelhamid432@gmail.com>
2025-09-16 13:20:52 +0300

    Add project files.

---
+++ Singleton/Start/LogMessage.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Singleton.Start;
+
+public class LogMessage
+{
+    public string Message { get; set; } = string.Empty;
+    public LogType LogType { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public override string ToString() => $"{LogType,7}  [{CreatedAt:yyyy-MM-dd hh:mm}] {Message}";
+}
